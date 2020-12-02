8bb099ef693930409e39ad461d7c0832078903f6
Alex Gamez <gamezalexandra@live.com>
2020-12-01 18:19:25 -0700

    first commit

---
+++ day_1.py
@@ -0,0 +1,13 @@
+
+f = open("expense_report.txt")
+
+
+fs = f.read().split('\n')
+i = 0
+for x in fs:
+    for y in fs:
+        x = int(x)
+        y = int(y)
+        sum = x+y
+        if sum == 2020:
+            print(x*y)
